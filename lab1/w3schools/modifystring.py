449bac421f94f7af7739f2a762b7e00e895627e0
dilnaz-zhumagulova <zhumagulova.d06@gmail.com>
2025-01-19 12:08:56 +0500

    modify string examples

--- lab1/w3schools/modifystring.py
+++ lab1/w3schools/modifystring.py
@@ -3,3 +3,12 @@ print(a.upper())
 
 a = "Hello, World!"
 print(a.lower())
+
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
