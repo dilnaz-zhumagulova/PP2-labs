e56c102d49e557f73f87b9264414cbf178567da1
dilnaz-zhumagulova <zhumagulova.d06@gmail.com>
2025-01-21 14:44:57 +0500

    operators exercises

---
+++ pp2 lab 2/opetators_exercises.py
@@ -0,0 +1,24 @@
+#1
+x = 5
+x += 3
+print(x)
+#8
+
+#2
+print(10*5)
+
+#3
+print(10/2)
+
+#4
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+   print("Yes, apple is a fruit!")
+  
+#5
+
+if 5 != 10:
+    print("5 and 10 is not equal")
+  
+if 5 == 10 or 4 == 4:
+    print("At least one of the statements is true")
