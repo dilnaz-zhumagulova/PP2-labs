f42e9fd6f96b587c0871c80c0eade0c4e1497828
dilnaz-zhumagulova <zhumagulova.d06@gmail.com>
2025-01-25 21:06:24 +0500

    change list items exercises done

---
+++ pp2 lab 2/change_list_items_execise.py
@@ -0,0 +1,15 @@
+#1
+mylist = ['apple', 'banana', 'cherry']
+mylist[0] = 'kiwi'
+print(mylist[1])
+#banana
+
+#2 Change the value from "apple" to "kiwi", in the fruits list.
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = 'kiwi'
+
+#3
+mylist = ['apple', 'banana', 'cherry']
+mylist[1:2] = ['kiwi', 'mango']
+print(mylist[2])
+#mango
