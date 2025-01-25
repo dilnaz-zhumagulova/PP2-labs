2e4a9e23637512066173b8eaa7a3ef3f0eeb15d9
dilnaz-zhumagulova <zhumagulova.d06@gmail.com>
2025-01-25 20:28:04 +0500

    lists exercise done

---
+++ pp2 lab 2/lists_exercise.py
@@ -0,0 +1,17 @@
+#1
+mylist = ['apple', 'banana', 'cherry']
+print(mylist[1])
+#banana
+
+#2
+mylist = ['apple', 'banana', 'banana', 'cherry']
+print(mylist[2])
+#banana
+
+#3
+#List items cannot be removed after the list has been created.
+#false
+
+#4
+thislist = ['apple', 'banana', 'cherry']
+print(len(thislist))
