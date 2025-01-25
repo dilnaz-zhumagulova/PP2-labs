6b57a4de7fcc66c1eff0b57c5178a94e1e436245
dilnaz-zhumagulova <zhumagulova.d06@gmail.com>
2025-01-25 21:11:39 +0500

    add list items examples doooone

--- pp2 lab 2/add_list_items.py
+++ pp2 lab 2/add_list_items.py
@@ -2,3 +2,22 @@ thislist = ["apple", "banana", "cherry"]
 thislist.append("orange")
 print(thislist)
 #['apple', 'banana', 'cherry', 'orange']
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+#['apple', 'orange', 'banana', 'cherry']
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+#['apple', 'banana', 'cherry', 'mango', 'pineapple', 'papaya']
+
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
+#The extend() method does not have to append lists, 
+#you can add any iterable object (tuples, sets, dictionaries etc.).
+#['apple', 'banana', 'cherry', 'kiwi', 'orange']
