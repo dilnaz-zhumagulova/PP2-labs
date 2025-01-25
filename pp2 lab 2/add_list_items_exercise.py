bfa533c079b9c03a83684e7fcbbe9da252e40d17
dilnaz-zhumagulova <zhumagulova.d06@gmail.com>
2025-01-25 21:16:51 +0500

    add list items execise done!!!

---
+++ pp2 lab 2/add_list_items_exercise.py
@@ -0,0 +1,18 @@
+#1
+mylist = ['apple', 'banana', 'cherry']
+mylist.insert(0, 'orange')
+print(mylist[1])
+#apple
+
+#2Use the append method to add "orange" to the fruits list.
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+
+#3 Use the insert method to add "lemon" as the second item in the fruits list.
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1, 'lemon')
+
+#4
+fruits = ['apple', 'banana', 'cherry']
+tropical = ['mango', 'pineapple', 'papaya']
+fruits.extend(tropical)
