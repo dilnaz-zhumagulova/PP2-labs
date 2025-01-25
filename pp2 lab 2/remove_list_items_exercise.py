25e39d022cb5e3a3d93efdca912ab2518d124c98
dilnaz-zhumagulova <zhumagulova.d06@gmail.com>
2025-01-25 21:23:09 +0500

    removing items of list exercise done

---
+++ pp2 lab 2/remove_list_items_exercise.py
@@ -0,0 +1,17 @@
+#1 
+#What is a List method for removing list items?
+#pop()
+
+#2 Use the remove method to remove "banana" from the fruits list.
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+
+#3
+mylist = ['apple', 'banana', 'cherry']
+mylist.pop(1)
+print(mylist)
+#['apple', 'cherry']
+
+#4 Select the correct function to remove all items from a list:
+fruits = ['apple', 'banana', 'cherry']
+fruits.clear()
