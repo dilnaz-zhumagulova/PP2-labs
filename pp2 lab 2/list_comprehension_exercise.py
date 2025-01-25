9aa2ddf7281592e4fde33d7070df9847c1044e55
dilnaz-zhumagulova <zhumagulova.d06@gmail.com>
2025-01-25 21:45:09 +0500

    list comprehension exercises

---
+++ pp2 lab 2/list_comprehension_exercise.py
@@ -0,0 +1,13 @@
+#1 What will be the value of newlist?
+fruits = ['apple', 'banana', 'cherry']
+newlist = [x for x in fruits if x == 'banana']
+#['banana']
+
+#2 Fill in the missing parts to complete the list comprehension:
+fruits = ["apple", "banana", "cherry"]
+newlist = [x for x in fruits]
+
+#3 What will be the value of newlist?
+fruits = ['apple', 'banana', 'cherry']
+newlist = ['apple' for x in fruits]
+#['apple', 'apple', 'apple']
