109fc01c66f634a5aff0e72512dd79927b71fec8
dilnaz-zhumagulova <zhumagulova.d06@gmail.com>
2025-01-25 20:36:21 +0500

    list items exercise examples

---
+++ pp2 lab 2/list_items_exercises.py
@@ -0,0 +1,17 @@
+#1
+mylist = ['apple', 'banana', 'cherry']
+print(mylist[-1])
+#cherry
+
+#2
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+#3
+mylist = ['apple', 'banana', 'cherry', 'orange', 'kiwi']
+print(mylist[1:4])
+#['banana', 'cherry', 'orange']
+
+#4
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
