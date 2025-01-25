e656c7ea07f0d2f98b7a8af5327415eb9a28e15c
dilnaz-zhumagulova <zhumagulova.d06@gmail.com>
2025-01-25 21:20:34 +0500

    removing list items examples

---
+++ pp2 lab 2/remove_list_items.py
@@ -0,0 +1,34 @@
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+#['apple', 'cherry']
+
+thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+thislist.remove("banana")
+print(thislist)
+#['apple', 'cherry', 'banana', 'kiwi']
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+#['apple', 'cherry']
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+#['apple', 'banana']
+
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+#['banana', 'cherry']
+
+thislist = ["apple", "banana", "cherry"]
+del thislist
+#The del keyword can also delete the list completely.
+
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
+#[]
+#The clear() method empties the list.
