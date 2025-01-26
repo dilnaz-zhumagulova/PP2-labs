1325191771e8c17bdbab7d4cc137bb9ef95da435
dilnaz-zhumagulova <zhumagulova.d06@gmail.com>
2025-01-26 23:30:21 +0500

    copy dictionaries exercises

---
+++ pp2 lab 2/dictionaries/copy_dict_ex.py
@@ -0,0 +1,7 @@
+#What is a correct syntax for making a copy of this dictionary:
+x = {'type' : 'fruit', 'name' : 'apple'}
+y = x.copy()
+#One of these codes is NOT a correct way of making a copy of a dictionary named x, which one:
+y = x.duplicate()
+#Copied dictionaries will not be able to change its item values.
+#False
