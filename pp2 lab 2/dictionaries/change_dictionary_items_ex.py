f280a881ea578833f8e7e7f27121c589333ad1a1
dilnaz-zhumagulova <zhumagulova.d06@gmail.com>
2025-01-26 23:12:22 +0500

    change dict items exercises

---
+++ pp2 lab 2/dictionaries/change_dictionary_items_ex.py
@@ -0,0 +1,15 @@
+#What is a correct syntax for changing the type from fruit to berry?
+x = {'type' : 'fruit', 'name' : 'banana'}
+#x['type'] = 'berry'
+
+#Change the "year" value from 1964 to 2020.
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["year"] = 2020
+
+#What is a correct syntax for changing the name from banana to apple?
+x = {'type' : 'fruit', 'name' : 'banana'}
+#x.update({'name': 'apple'})
