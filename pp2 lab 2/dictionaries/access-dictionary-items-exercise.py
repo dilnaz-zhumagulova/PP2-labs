679442e57a38ec971d5aa7491ee6032117e3bb9a
dilnaz-zhumagulova <zhumagulova.d06@gmail.com>
2025-01-26 23:08:11 +0500

    access dictionary items exercise modified

---
+++ pp2 lab 2/dictionaries/access-dictionary-items-exercise.py
@@ -0,0 +1,14 @@
+#You can access item values by referring to the key name.
+#True
+
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(car.get("model"))
+
+#What will be the printed result?
+x = {'type' : 'fruit', 'name' : 'banana'}
+print(x['type'])
+#fruit
