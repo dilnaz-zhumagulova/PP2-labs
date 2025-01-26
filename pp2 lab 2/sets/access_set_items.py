60864d4b116e41d54c457e35040dfb565f336279
dilnaz-zhumagulova <zhumagulova.d06@gmail.com>
2025-01-26 16:31:24 +0500

    access set items examples

---
+++ pp2 lab 2/sets/access_set_items.py
@@ -0,0 +1,18 @@
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+
+#cherry
+#banana
+#apple
+
+thisset = {"apple", "banana", "cherry"}
+print("banana" in thisset)
+#true
+
+thisset = {"apple", "banana", "cherry"}
+print("banana" not in thisset)
+#false
+
+#Once a set is created, you cannot change its items, but you can add new items.
