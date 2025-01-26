4a7b34e35f10ce58fe1f729a4e805923afd60eea
dilnaz-zhumagulova <zhumagulova.d06@gmail.com>
2025-01-26 22:56:15 +0500

    dictionary exercises

---
+++ pp2 lab 2/dictionaries/dictionary_exercise.py
@@ -0,0 +1,11 @@
+#Which one of these is a dictionary?
+x = {'type' : 'fruit', 'name' : 'banana'}
+
+#Dictionary items cannot be removed after the dictionary has been created.
+#false
+
+#A dictionary cannot have two keys with the same name
+#true
+
+x = {'type' : 'fruit', 'name' : 'banana'}
+print(len(x))
