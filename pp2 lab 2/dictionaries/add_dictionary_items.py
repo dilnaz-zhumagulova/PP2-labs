06f5a8221b3cf2bf331b405833f5b393d4b8b312
dilnaz-zhumagulova <zhumagulova.d06@gmail.com>
2025-01-26 23:14:42 +0500

    add dictionary items examples

--- pp2 lab 2/dictionaries/add_dictionary_items.py
+++ pp2 lab 2/dictionaries/add_dictionary_items.py
@@ -5,7 +5,7 @@ thisdict =  {
 }
 thisdict["color"] = "red"
 print(thisdict)
-​#{'brand': 'Ford', 'model': 'Mustang', 'year': 1964, 'color': 'red'}
+#{'brand': 'Ford', 'model': 'Mustang', 'year': 1964, 'color': 'red'}
 
 #The update() method will update the dictionary with the items from a given argument. If the item does not exist, the item will be added.
 thisdict = {
