5c106be511576326a2fbde0d315c5e56644635e2
dilnaz-zhumagulova <zhumagulova.d06@gmail.com>
2025-01-26 15:46:12 +0500

    join tuples examples

---
+++ pp2 lab 2/tuples/join_tuples.py
@@ -0,0 +1,13 @@
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+#('a', 'b', 'c', 1, 2, 3)
+
+#multiply tables
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+
+print(mytuple)
+#('apple', 'banana', 'cherry', 'apple', 'banana', 'cherry')
