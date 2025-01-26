8c840ee28f368572a288567ce90d8f6c2037e27e
dilnaz-zhumagulova <zhumagulova.d06@gmail.com>
2025-01-26 23:27:26 +0500

    loop dict exercises

---
+++ pp2 lab 2/dictionaries/loop_dict-ex.py
@@ -0,0 +1,18 @@
+#What is a correct syntax for looping through the values of this dictionary:
+x = {'type' : 'fruit', 'name' : 'apple'}
+for y in x.values():
+  print(y)
+
+#What is a correct syntax for looping through the keys AND values of this dictionary:
+x = {'type' : 'fruit', 'name' : 'apple'}
+for y, z in x.items():
+  print(y, z)
+
+myset = {'apple', 'banana', 'cherry'}
+for x in myset:
+  print(x)
+
+#What is a correct syntax for looping through the keys of this dictionary:
+x = {'type' : 'fruit', 'name' : 'apple'}
+for y in x.keys():
+  print(y)
