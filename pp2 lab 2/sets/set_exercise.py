685d2b49399fb0ce60b0f92d1ea94625a347f882
dilnaz-zhumagulova <zhumagulova.d06@gmail.com>
2025-01-26 16:29:19 +0500

    set exercises

---
+++ pp2 lab 2/sets/set_exercise.py
@@ -0,0 +1,11 @@
+#Which one of these is a set?
+myset = {'apple', 'banana', 'cherry'}
+
+#Set items cannot be removed after the set has been created.
+#false
+
+#A set cannot have two items with the same value.
+#true
+
+fruits = {'apple', 'banana', 'cherry'}
+print(len(fruits))
