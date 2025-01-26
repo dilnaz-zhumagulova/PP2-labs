a41cfdd74dd2916afad61c5e8ce67d967009bdc7
dilnaz-zhumagulova <zhumagulova.d06@gmail.com>
2025-01-26 16:33:10 +0500

    access set items exercises

---
+++ pp2 lab 2/sets/access_set_items_exercise.py
@@ -0,0 +1,12 @@
+#You can access set items by referring to the index.
+#false
+
+#Check if "apple" is present in the fruits set.
+fruits = {"apple", "banana", "cherry"}
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+  
+#What will be the printed result?
+thisset = {'apple', 'banana', 'cherry'}
+print('banana' not in thisset)
+#False
