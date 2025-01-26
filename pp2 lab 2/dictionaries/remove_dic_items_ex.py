ac5c0e81e52dddc413b36de80227a60c8d164d25
dilnaz-zhumagulova <zhumagulova.d06@gmail.com>
2025-01-26 23:22:33 +0500

    remove dict items exercises

---
+++ pp2 lab 2/dictionaries/remove_dic_items_ex.py
@@ -0,0 +1,19 @@
+#what is a dictionary method for removing an item from a dictionary?
+#pop()
+
+#Use the pop method to remove "model" from the car dictionary.
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.pop("model")
+
+#Use the clear method to empty the car dictionary.
+
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.clear()
